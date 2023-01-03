8295a21697aadac7f6c4c486a06479a03b8eca10
Preschian Febryantara <preschian27@gmail.com>
2023-01-03 23:11:52 +0700

    feat: serve using cf images or r2 bucket

--- workers/cdn/src/index.ts
+++ workers/cdn/src/index.ts
@@ -1,5 +1,4 @@
 import { Hono } from 'hono'
-import { cache } from 'hono/cache'
 import { cors } from 'hono/cors'
 
 interface Env {
@@ -7,31 +6,87 @@ interface Env {
   DEDICATED_GATEWAY: string;
   DEDICATED_BACKUP_GATEWAY: string;
   CLOUDFLARE_GATEWAY: string;
+  BUCKET_PUBLIC_URL: string;
+  CF_IMAGE_ACCOUNT: string;
+  CF_IMAGE_ID: string;
+
+  // wrangler secret
+  TOKEN: string;
 }
 
 const app = new Hono<{ Bindings: Env }>()
 
-app.use('*', cors())
+app.use('/ipfs/*', cors())
 
 app.get('/', (c) => c.text('Hello! cf-workers!'))
-
-// app.get('/ipfs/*', cache({ cacheName: 'cdn-010323-01', cacheControl: 'max-age=31536000' }))
 
 app.all('/ipfs/:cid', async (c) => {
   const cid = c.req.param('cid')
   const method = c.req.method
 
-  if (method === 'GET' || method === 'HEAD') {
+  if (method === 'GET') {
+    // TODO: get/set cache before hit ipfs gateway
     const fetchIPFS = await Promise.any([
       fetch(`${c.env.DEDICATED_GATEWAY}/ipfs/${cid}`),
       fetch(`${c.env.DEDICATED_BACKUP_GATEWAY}/ipfs/${cid}`)
     ])
-    // const statusCode = fetchIPFS.status
-    const contentType = fetchIPFS.headers.get('Content-Type')
+    const statusCode = fetchIPFS.status
 
-    c.header('Content-Type', contentType || 'text/plain')
+    if (statusCode === 200) {
+      const objectName = `ipfs/${cid}`
+      const object = await c.env.MY_BUCKET.get(objectName);
 
-    // return c.text(`${statusCode}: ${cid}`)
+      // upload to r2
+      if (object === null) {
+        await c.env.MY_BUCKET.put(objectName, fetchIPFS.body, {
+          httpMetadata: fetchIPFS.headers,
+        })
+      }
+
+      // start upload to cf-images section
+      const uploadHeaders = new Headers();
+      uploadHeaders.append("Authorization", `Bearer ${c.env.TOKEN}`);
+
+      const uploadFormData = new FormData();
+      uploadFormData.append("url", `${c.env.DEDICATED_GATEWAY}/ipfs/${cid}`);
+      uploadFormData.append("id", cid);
+
+      const requestOptions = {
+        method: 'POST',
+        headers: uploadHeaders,
+        body: uploadFormData,
+        redirect: 'follow'
+      };
+
+      const uploadCfImage = await fetch(`https://api.cloudflare.com/client/v4/accounts/${c.env.CF_IMAGE_ACCOUNT}/images/v1`, requestOptions)
+      const uploadStatus = uploadCfImage.status
+
+      // end upload to cf-images section
+      if (uploadStatus === 200 || uploadStatus === 409) {
+        const cdn = await fetch(`https://imagedelivery.net/${c.env.CF_IMAGE_ID}/${cid}/public`)
+        const cdnContentType = cdn.headers.get('content-type') || 'text/plain'
+        c.header('content-type', cdnContentType)
+        return c.body(cdn.body)
+      }
+
+      const r2Object = await fetch(`${c.env.BUCKET_PUBLIC_URL}/ipfs/${cid}`)
+      if (r2Object.status === 200) {
+        const r2ContentType = r2Object.headers.get('content-type') || 'text/plain'
+        c.header('content-type', r2ContentType)
+        return c.body(r2Object.body)
+      }
+    }
+
+    // fallback to cf-ipfs
+    return Response.redirect(`${c.env.CLOUDFLARE_GATEWAY}/ipfs/${cid}`, 302)
+  }
+
+  if (method === 'HEAD') {
+    const fetchIPFS = await Promise.any([
+      fetch(`${c.env.DEDICATED_GATEWAY}/ipfs/${cid}`),
+      fetch(`${c.env.DEDICATED_BACKUP_GATEWAY}/ipfs/${cid}`)
+    ])
+
     return c.body(fetchIPFS.body)
   }
 })
