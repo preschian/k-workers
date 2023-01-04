746468e68a74e0494b51fb1a777bfe2e3df041fe
Preschian Febryantara <preschian27@gmail.com>
2023-01-04 13:46:22 +0700

    feat: move to utils

--- workers/cdn/src/index.ts
+++ workers/cdn/src/index.ts
@@ -1,6 +1,8 @@
 import { Hono } from 'hono';
 import { cors } from 'hono/cors';
 
+import { uploadToCloudflareImages } from './utils/cloudflare-images';
+
 interface Env {
   MY_BUCKET: R2Bucket;
   DEDICATED_GATEWAY: string;
@@ -15,51 +17,9 @@ interface Env {
 
 const app = new Hono<{ Bindings: Env }>();
 
-app.use('/ipfs/*', cors());
-
 app.get('/', (c) => c.text('Hello! cf-workers!'));
 
-async function uploadToCloudflareImages({
-  token,
-  gateway,
-  cid,
-  imageAccount,
-  imageId,
-}: {
-  token: string;
-  gateway: string;
-  cid: string;
-  imageAccount: string;
-  imageId: string;
-}) {
-  const uploadHeaders = new Headers();
-  uploadHeaders.append('Authorization', `Bearer ${token}`);
-
-  const uploadFormData = new FormData();
-  uploadFormData.append('url', `${gateway}/ipfs/${cid}`);
-  uploadFormData.append('id', cid);
-
-  const requestOptions = {
-    method: 'POST',
-    headers: uploadHeaders,
-    body: uploadFormData,
-    redirect: 'follow',
-  };
-
-  const uploadCfImage = await fetch(
-    `https://api.cloudflare.com/client/v4/accounts/${imageAccount}/images/v1`,
-    requestOptions
-  );
-  const uploadStatus = uploadCfImage.status;
-
-  // if image supported by cf-images or already exists, redirect to cf-images
-  if (uploadStatus === 200 || uploadStatus === 409) {
-    // return Response.redirect(`https://imagedelivery.net/${c.env.CF_IMAGE_ID}/${cid}/public`, 302)
-    return `https://imagedelivery.net/${imageId}/${cid}/public`;
-  }
-
-  return '';
-}
+app.use('/ipfs/*', cors());
 
 app.all('/ipfs/:cid', async (c) => {
   const cid = c.req.param('cid');
