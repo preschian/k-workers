c1ca93ea7c74016b8049e00b391dcd66b05c5577
Preschian Febryantara <preschian27@gmail.com>
2023-02-15 20:20:14 +0700

    feat: mock some status

---
+++ workers/my-app/src/index.ts
@@ -0,0 +1,30 @@
+import { Hono } from 'hono';
+import { poweredBy } from 'hono/powered-by';
+
+const app = new Hono();
+
+app.use('*', poweredBy());
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!');
+});
+
+app.get('/404', (c) => {
+  c.status(404);
+  return c.body('Not Found');
+});
+
+app.get('/429', (c) => {
+  c.status(429);
+  return c.body('Too Many Requests');
+});
+
+app.get('/delay', (c) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(c.text('Delayed'));
+    }, 3000);
+  });
+});
+
+export default app;
