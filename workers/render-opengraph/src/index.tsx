a766449b54a48d15d987633bf2dc6aefa4698412
Preschian Febryantara <preschian27@gmail.com>
2023-03-21 18:37:00 +0700

    feat: fetch metadata

--- workers/render-opengraph/src/index.tsx
+++ workers/render-opengraph/src/index.tsx
@@ -1,55 +1,54 @@
 import { Hono } from 'hono';
-import { html } from 'hono/html';
+
+import { Opengraph } from './template';
 
 const app = new Hono();
-
-interface SiteData {
-  children?: any;
-  title: string;
-  description: string;
-  canonical: string;
-  image: string;
-}
-
-const Layout = (props: SiteData) => html`
-  <!DOCTYPE html>
-  <html>
-    <head>
-      <title>${props.title}</title>
-
-      <link rel="canonical" href="${props.canonical}" />
-
-      <meta name="description" content="${props.description}" />
-
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content="${props.canonical}" />
-      <meta
-        property="og:site_name"
-        content="KodaDot - Polkadot / Kusama NFT explorer"
-      />
-      <meta property="og:title" content="${props.title}" />
-      <meta property="og:description" content="${props.description}" />
-      <meta property="og:image" content="${props.image}" />
-
-      <meta name="twitter:card" content="summary_large_image" />
-    </head>
-
-    <body>
-      ${props.children}
-    </body>
-  </html>
-`;
-
-const Opengraph = (props: { siteData: SiteData; name: string }) => (
-  <Layout {...props.siteData}>
-    <h1>{props.siteData.title}</h1>
-    <img src={props.siteData.image} alt={props.siteData.title} />
-  </Layout>
-);
 
 app.get('/', (c) => {
   return c.text('hello hono.js');
 });
+
+function ipfsToCdn(ipfs: string) {
+  const ipfsCid = ipfs.split('ipfs:/')[1];
+  const cdn = new URL(ipfsCid, 'https://image.w.kodadot.xyz');
+
+  return cdn.toString();
+}
+
+function formatPrice(price: string) {
+  const number = price;
+  const numAsNumber = parseFloat(number);
+  const divisor = 1000000000000; // 10^12
+  const convertedValue = numAsNumber / divisor;
+  const ksmValue = convertedValue.toFixed(1);
+  return number === '0' ? '' : `${ksmValue} KSM`;
+}
+
+async function getProperties(nft) {
+  if (!nft.meta) {
+    const response = await fetch(ipfsToCdn(nft.metadata));
+    const data = await response.json();
+
+    return {
+      name: data.name,
+      description: data.description,
+      title: `${data.name} | Low Carbon NFTs`,
+      cdn: ipfsToCdn(data.image),
+    };
+  }
+
+  const name = nft.name;
+  const description = nft.meta?.description;
+  const title = `${name} | Low Carbon NFTs`;
+  const cdn = ipfsToCdn(nft.meta?.image);
+
+  return {
+    name,
+    description,
+    title,
+    cdn,
+  };
+}
 
 app.get('/:chain/gallery/:id', async (c) => {
   const chain = c.req.param('chain') as 'bsx' | 'rmrk';
@@ -89,24 +88,15 @@ app.get('/:chain/gallery/:id', async (c) => {
   const { nftEntityById } = data.data;
 
   const canonical = `https://kodadot.xyz/${chain}/gallery/${id}`;
-  const name = nftEntityById.name;
-  const description = nftEntityById.meta?.description;
-  const title = `${name} | Low Carbon NFTs`;
+  const { name, description, title, cdn } = await getProperties(nftEntityById);
 
   // contruct price
-  const number = nftEntityById.price;
-  const numAsNumber = parseFloat(number);
-  const divisor = 1000000000000; // 10^12
-  const convertedValue = numAsNumber / divisor;
-  const ksmValue = convertedValue.toFixed(1);
-  const price = number === '0' ? '' : `${ksmValue} KSM`;
+  const price = formatPrice(nftEntityById.price);
 
-  // construct image to cdn
-  const ipfsCid = nftEntityById.meta?.image.split('ipfs:/')[1];
-  const cdn = new URL(ipfsCid, 'https://image.w.kodadot.xyz');
+  // construct vercel image with cdn
   const image = new URL(`https://og-image-green-seven.vercel.app/${name}.jpeg`);
   image.searchParams.set('price', price);
-  image.searchParams.set('image', cdn.toString());
+  image.searchParams.set('image', cdn);
 
   const props = {
     name: `${chain} ${id}`,
